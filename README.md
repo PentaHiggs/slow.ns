ee5048e35ae11ae3aafbff1decb8825bae9f08ed
Andy Perez <ichiroska89@yahoo.com>
2017-09-11 20:00:25 -0400

    Added and tested shell script to split kaggle datasets for use with keras

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # slow.ns
-Collection of scripts, assignments, notes, etc. for fast.ai (In case you were wondering, the repo name is slow . natural stupidity.  heh )
+Collection of scripts, assignments, notes, etc. for fast.ai 
+(In case you were wondering, the repo name is slow . natural stupidity.  heh )
+
+## kaggle_splitter.sh
+Script to split kaggle training sets into folders for each class and a 90/10 train/validation
+split, as well as a smaller subsample (of size 100 for every category).
+
+### Usage
+```
+$ kaggle_splitter dirname cat_1 cat_2 ...
+```
+Where dirname is the name of the directory where .zip folders from kaggle were unzipped;
+We are splitting the train.zip files into the directory tree shown below.
+cat_1, cat_2, ... are the category names (no actual ellipses are to be used in the command above)
+It is assumed that file names will be of the form cat_1.\*, cat_2.\*, etc.
+
+### Directory Structure
+dirname/
+	|----/train
+    |       |----/cat_1
+	|		|----/cat_2
+	|			.
+	|			.
+	|			.
+	|----/valid
+	|		|----/cat_1
+	|		|----/cat_2
+	|			.
+	|			.
+	|			.
+	|----/sample
+			|----/train
+			|		|----/cat_1
+			|		|----/cat_2
+			|				.
+			|				.
+			|				.
+			|----/valid
+					|----/cat_1
+					|----/cat_2
+						.
+						.
+						.						
+
+
---
+++ kaggle_splitter.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+# Usage:
+# If data is in directory catsdogs, and the categories are cat and dog, then proper command is
+# $ kaggle_splitter.sh catsdogs cat dog
+# 
+# Future version will hopefully decipher category names from file names without manual input
+
+echo "Creating train / valid / sample splits for "$1
+
+# Initialize directory skeleton (without categories)
+# Train should already exist.
+mkdir $1/valid $1/sample
+mkdir $1/sample/train $1/sample/valid
+
+for var in ${@:2}
+do
+	mkdir $1/train/$var $1/valid/$var
+	mkdir $1/sample/train/$var $1/sample/valid/$var
+	
+	echo "Moving $var files to directory"
+	# Move all files in category $var to $1/$var.  They are initially all in $1/train/
+	mkdir $1/$var
+	cd $1/train/
+	find . -name $var".*" -exec mv -v -t ../$var/ {} +
+	#find $1/train/ -name $var".*" -exec mv -v -t ../$var {} +
+	cd ../../
+
+	echo "Copying $var into sample"
+	# Copy some into sample
+	mkdir $1/sample/$var
+	for file in $(ls -p $1/$var | grep -v / | tail -100 )
+	do	
+		cp $1/$var/$file $1/sample/$var/
+	done
+
+	echo "Splitting $var sample"
+	# Train/valid split sample
+	for file in $(ls -p $1/sample/$var | grep -v / | tail -10)
+	do
+		mv $1/sample/$var/$file $1/sample/valid/$var
+	done
+	
+	echo "Emptying $1/sample/$var"
+	# Send rest into $1/sample/$var into train
+	for file in $(ls -p $1/sample/$var | grep -v /)
+	do
+		mv $1/sample/$var/$file $1/sample/train/$var
+	done
+	
+	# Delete now empty temporary directory
+	rmdir $1/sample/$var
+
+	echo "Calculating train/valid split for main data for $var"
+	# Train/valid split regular, 10% validation
+	numfiles=$(ls 2>/dev/null -Ub1 -- * | wc -l)
+	numvalid=$(($numfiles/10))
+
+	echo "Moving validation set for $var"
+	for file in $(ls -p $1/$var | grep -v / | tail -$numvalid)
+	do
+		mv $1/$var/$file $1/valid/$var
+	done
+
+	echo "Emptying $1/$var "
+	# Send rest into $1/train/$var
+	for file in $(ls -p $1/$var | grep -v / )
+	do	
+		mv $1/$var/$file $1/train/$var
+	done
+	
+	# This directory should now be empty
+	rmdir $1/$var
+done
